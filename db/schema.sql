2c1275688de226cf9172efb28250dd8690d09dcd
Ryan Tam <ryanjtam@gmail.com>
2020-04-20 00:59:15 -0700

    setting up jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ USE flavortown;
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    hide BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
